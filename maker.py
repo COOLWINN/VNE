b8edb1aa63358b524ef4d2515a9b20893a66a51d
gooyunn <gooyunn@qq.com>
2019-01-24 12:21:57 +0800

    we can customize the range of resource capacity in generate_network() function

--- maker.py
+++ maker.py
@@ -8,10 +8,7 @@ import networkx as nx
 # 仿真时间
 TOTAL_TIME = 50000
 
-# 生成网络所需的参数
 SCALE = 100
-NODE_CAPACITY = 100
-LINK_CAPACITY = 100
 
 # 仅与虚拟网络请求相关的参数
 DURATION_MEAN = 1000
@@ -34,59 +31,87 @@ if not os.path.exists(save_path):
     os.makedirs(save_path)
 
 
-def dis(coordinate1, coordinate2):
+def calculate_dis(coordinate1, coordinate2):
     """给定两个节点坐标，求解它们之间的欧氏距离"""
     return math.sqrt(pow(coordinate1[0] - coordinate2[0], 2) + pow(coordinate1[1] - coordinate2[1], 2))
 
 
-def make_sub_wm(nodes_number, connect_prob):
+def generate_network(network_name, min_res, max_res, time=0, duration=0, transit_nodes=0):
+    """生成网络文件"""
+
+    # Step1: 执行itm指令生成.gb文件
+    spec_filename = 'spec-%s' % network_name
+    cmd = "%s %s" % (itm, spec_dir + spec_filename)
+    os.system(cmd)
+
+    # Step2: 执行sgb2alt指令将刚刚生成的gb文件转换为alt文件
+    gb_filename = spec_filename + '-0.gb'
+    alt_filename = '%s.alt' % network_name
+    cmd = "%s %s %s" % (sgb2alt, spec_dir + gb_filename, alt_dir + alt_filename)
+    os.system(cmd)
+
+    # Step3: 读取刚刚生成的alt文件
+    with open(alt_dir + alt_filename) as f:
+        lines = f.readlines()
+
+    # Step4: 生成网络文件
+    print("generate %s" % network_name)
+    network_filename = '%s.txt' % network_name
+    with open(save_path + network_filename, 'w') as network_file:
+
+        coordinates = []
+
+        # Step4-1: 写入网络整体信息
+        line_one = lines[1].split()
+        num_nodes = int(line_one[0])
+        num_edges = int(int(line_one[1]) / 2)
+        if network_name == 'sub-wm' or network_name == 'sub-ts':
+            # 物理拟网络信息包括：节点数量、链路数量
+            network_file.write("%d %d\n" % (num_nodes, num_edges))
+        else:
+            # 虚拟网络信息包括：节点数量、链路数量、到达时间、持续时间、可映射范围
+            network_file.write("%d %d %d %d %d\n" % (num_nodes, num_edges, time, duration, MAX_DISTANCE))
+
+        # Step4-2: 依次写入节点信息（x坐标，y坐标，节点资源）
+        for line in lines[4:4 + num_nodes]:
+            blocks = line.split()
+            x = int(blocks[2])
+            y = int(blocks[3])
+            coordinates.append((x, y))
+            resource = random.uniform(min_res, max_res)
+            if network_name == 'sub-ts' and len(coordinates) <= transit_nodes:
+                network_file.write("%d %d %f\n" % (x, y, 200 + resource))
+                continue
+            network_file.write("%d %d %f\n" % (x, y, resource))
+
+        # Step4-3: 依次写入链路信息（起始节点，终止节点，带宽资源，时延）
+        for line in lines[-num_edges:]:
+            from_id, to_id, length, a = [int(x) for x in line.split()]
+            distance = calculate_dis(coordinates[from_id], coordinates[to_id])
+            resource = random.uniform(min_res, max_res)
+            if network_name == 'sub-ts':
+                if from_id < transit_nodes and to_id < transit_nodes:
+                    network_file.write("%d %d %f %f\n" % (from_id, to_id, 200 + resource, distance))
+                    continue
+                if from_id < transit_nodes or to_id < transit_nodes:
+                    network_file.write("%d %d %f %f\n" % (from_id, to_id, 100 + resource, distance))
+                    continue
+            network_file.write("%d %d %f %f\n" % (from_id, to_id, resource, distance))
+
+
+def make_sub_wm(nodes_number, connect_prob, min_res, max_res):
     """生成物理网络文件（基于waxman随机型网络模型）"""
 
-    # Step1: 生成GT-ITM配置文件
-    spec_filename = 'itm-spec-sub'
+    network_name = 'sub-wm'
+
+    # 生成GT-ITM配置文件
+    spec_filename = 'spec-%s' % network_name
     with open(spec_dir + spec_filename, 'w') as f:
         f.write("geo 1\n")
         f.write("%d %d 2 %f 0.2\n" % (nodes_number, SCALE, connect_prob))
 
-    # Step2: 执行itm指令生成.gb文件
-    cmd = "%s %s" % (itm, spec_dir + spec_filename)
-    os.system(cmd)
-
-    # Step3: 执行sgb2alt指令将刚刚生成的gb文件转换为alt文件
-    gb_filename = spec_filename + '-0.gb'
-    alt_filename = 'sub.alt'
-    cmd = "%s %s %s" % (sgb2alt, spec_dir + gb_filename, alt_dir + alt_filename)
-    os.system(cmd)
-
-    # Step4: 读取刚刚生成的alt文件
-    with open(alt_dir + alt_filename) as f:
-        lines = f.readlines()
-
-    # Step5: 生成物理网络并存储为指定文件
-    sub_filename = 'sub.txt'
-    with open(save_path + sub_filename, 'w') as sub_file:
-
-        coordinates = []
-
-        # Step5-1: 写入物理网络的节点数量和链路数量
-        a, b, c, d = [x for x in lines[1].split()]
-        num_nodes = int(a)
-        num_edges = int(int(b) / 2)
-        sub_file.write("%d %d\n" % (num_nodes, num_edges))
-
-        # Step5-2: 依次写入节点信息（x坐标，y坐标，拥有的节点资源）
-        for line in lines[4:4 + num_nodes]:
-            t1, t2, x, y = [int(x) for x in line.split()]
-            coordinates.append((x, y))
-            resource = random.uniform(1, 2) * NODE_CAPACITY * 0.5
-            sub_file.write("%d %d %f\n" % (x, y, resource))
-
-        # Step5-3: 依次写入链路信息（起始节点，终止节点，拥有的带宽资源，长度）
-        for line in lines[-num_edges:]:
-            from_id, to_id, length, a = [int(x) for x in line.split()]
-            resource = random.uniform(1, 2) * LINK_CAPACITY * 0.5
-            distance = dis(coordinates[from_id], coordinates[to_id])
-            sub_file.write("%d %d %f %f\n" % (from_id, to_id, resource, distance))
+    # 生成基于Waxman随机模型的物理网络文件
+    generate_network(network_name, min_res, max_res)
 
 
 # transits： transit域数量
@@ -95,11 +120,13 @@ def make_sub_wm(nodes_number, connect_prob):
 # transit_p: transit域内的连通性
 # stub_nodes: 每个stub域中节点数量
 # stub_p: stub域内的连通性
-def make_sub_ts(transits, stubs, transit_nodes, transit_p, stub_nodes, stub_p):
+def make_sub_ts(transits, stubs, transit_nodes, transit_p, stub_nodes, stub_p, min_res, max_res):
     """生成物理网络文件（基于Transit-Stub模型）"""
 
+    network_name = 'sub-ts'
+
     # Step1: 生成GT-ITM配置文件
-    spec_filename = 'itm-spec-sub-ts'
+    spec_filename = 'spec-%s' % network_name
     with open(spec_dir + spec_filename, 'w') as f:
         f.write("ts 1 47\n")
         f.write("%d 0 0\n" % stubs)
@@ -107,63 +134,29 @@ def make_sub_ts(transits, stubs, transit_nodes, transit_p, stub_nodes, stub_p):
         f.write("%d 5 3 %f\n" % (transit_nodes, transit_p))
         f.write("%d 5 3 %f\n" % (stub_nodes, stub_p))
 
-    # Step2: 执行itm指令生成.gb文件
-    cmd = "%s %s" % (itm, spec_dir + spec_filename)
-    os.system(cmd)
+    # 生成基于transit-stub模型的物理网络文件
+    generate_network('sub-ts', min_res, max_res, transit_nodes=transit_nodes)
 
-    # Step3: 执行sgb2alt指令将刚刚生成的gb文件转换为alt文件
-    gb_filename = spec_filename + '-0.gb'
-    alt_filename = 'sub-ts.alt'
-    cmd = "%s %s %s" % (sgb2alt, spec_dir + gb_filename, alt_dir + alt_filename)
-    os.system(cmd)
 
-    # Step4: 读取刚刚生成的alt文件
-    with open(alt_dir + alt_filename) as f:
-        lines = f.readlines()
+def make_req(index, min_res, max_res, node_amount, time, duration):
+    """生成虚拟网络请求文件"""
 
-    # Step5: 生成物理网络并存储为指定文件
-    sub_filename = 'sub-ts.txt'
-    with open(save_path + sub_filename, 'w') as sub_file:
+    network_name = 'req%d' % index
 
-        coordinates = []
+    # 生成GT-ITM配置文件
+    spec_filename = 'spec-%s' % network_name
+    with open(spec_dir + spec_filename, 'w') as f:
+        f.write("geo 1\n")
+        f.write("%d %d 2 0.5 0.2\n" % (node_amount, SCALE))
 
-        # Step5-1: 写入物理网络的节点数量和链路数量
-        a, b, c, d, e, f, g = [x for x in lines[1].split()]
-        num_nodes = int(a)
-        num_edges = int(int(b) / 2)
-        sub_file.write("%d %d\n" % (num_nodes, num_edges))
-
-        # Step5-2: 依次写入节点信息（x坐标，y坐标，拥有的节点资源）
-        count = 0
-        for line in lines[4:4 + num_nodes]:
-            count += 1
-            t1, t2, x, y = [x for x in line.split()]
-            x = int(x)
-            y = int(y)
-            coordinates.append((x, y))
-            resource = random.uniform(1, 2) * NODE_CAPACITY * 0.5
-            if count <= transit_nodes:
-                sub_file.write("%d %d %f\n" % (x, y, 200 + resource))
-            else:
-                sub_file.write("%d %d %f\n" % (x, y, resource))
-
-        # Step5-3: 依次写入链路信息（起始节点，终止节点，拥有的带宽资源，长度）
-        for line in lines[-num_edges:]:
-            from_id, to_id, length, a = [int(x) for x in line.split()]
-            resource = random.uniform(1, 2) * LINK_CAPACITY * 0.5
-            distance = dis(coordinates[from_id], coordinates[to_id])
-            if from_id < transit_nodes and to_id < transit_nodes:
-                sub_file.write("%d %d %f %f\n" % (from_id, to_id, 200 + resource, distance))
-            elif from_id < transit_nodes or to_id < transit_nodes:
-                sub_file.write("%d %d %f %f\n" % (from_id, to_id, 100 + resource, distance))
-            else:
-                sub_file.write("%d %d %f %f\n" % (from_id, to_id, resource, distance))
+    # 生成虚拟网络文件
+    generate_network(network_name, min_res, max_res, time=time, duration=duration)
 
 
 # possion_mean：虚拟网络请求的到达服从泊松分布，且平均每1000个时间单位内到达的数量为possion_mean个
 # 虚拟节点数量服从[min_num_nodes, max_num_nodes]的均匀分布
-def make_req(possion_mean, min_num_nodes, max_num_nodes):
-    """生成虚拟网络请求文件"""
+def make_batch_req(possion_mean, min_num_nodes, max_num_nodes, min_res, max_res):
+    """生成多个虚拟网络请求文件"""
 
     # 时间间隔
     interval = 1000
@@ -181,68 +174,19 @@ def make_req(possion_mean, min_num_nodes, max_num_nodes):
     # 按照以下步骤分别生成req_num个虚拟网络请求文件
     for i in range(req_num):
 
-        # Step1: 生成GT-ITM配置文件
-        spec_filename = 'itm-spec%d' % i
-        with open(spec_dir + spec_filename, 'w') as f:
-            f.write("geo 1\n")
-            t = random.randint(min_num_nodes, max_num_nodes)
-            f.write("%d %d 2 0.5 0.2\n" % (t, SCALE))
+        if count_k == k:
+            k = 0
+            while k == 0:
+                k = np.random.poisson(possion_mean)
+            count_k = 0
+            start = p * interval
+            p += 1
+        count_k += 1
 
-        # Step2: 执行itm指令生成gb文件
-        cmd = "%s %s" % (itm, spec_dir + spec_filename)
-        os.system(cmd)
-
-        # Step3: 执行sgb2alt指令将刚刚生成的gb文件转换为alt文件
-        gb_filename = spec_filename + '-0.gb'
-        alt_filename = '%d.alt' % i
-        cmd = '%s %s %s' % (sgb2alt, spec_dir + gb_filename, alt_dir + alt_filename)
-        os.system(cmd)
-
-        # Step4: 读取生成的alt文件
-        with open(alt_dir + alt_filename) as f:
-            lines = f.readlines()
-
-        # Step5: 生成虚拟网络请求并存储为指定文件
-        print("generate req%d" % i)
-        req_filename = 'req%d.txt' % i
-        with open(save_path + req_filename, 'w') as req_file:
-
-            coordinates = []
-
-            a, b, c, d = [x for x in lines[1].split()]
-
-            # Step5-1: 写入虚拟网络请求整体信息（节点数量、链路数量、到达时间、持续时间、映射范围）
-            # ①虚拟网络请求的节点数
-            num_nodes = int(a)
-            # ②虚拟网络请求的链路数
-            num_edges = int(int(b) / 2)
-            # ③虚拟网络请求的到达时间（保证虚拟网络请求的到达服从泊松分布）
-            if count_k == k:
-                k = 0
-                while k == 0:
-                    k = np.random.poisson(possion_mean)
-                count_k = 0
-                start = p * interval
-                p += 1
-            count_k += 1
-            time = start + ((count_k + 1) / (k + 1)) * interval
-            # ④虚拟网络请求的持续时间（服从指数分布）
-            duration = MIN_DURATION + int(-math.log(random.random()) * (DURATION_MEAN - MIN_DURATION))
-            req_file.write("%d %d %d %d %d\n" % (num_nodes, num_edges, time, duration, MAX_DISTANCE))
-
-            # Step5-2: 依次写入节点信息（x坐标，y坐标，请求的节点资源）
-            for line in lines[4:4 + num_nodes]:
-                t1, t2, x, y = [int(x) for x in line.split()]
-                coordinates.append((x, y))
-                resource = random.random() * NODE_CAPACITY * 0.5
-                req_file.write("%d %d %f\n" % (x, y, resource))
-
-            # Step5-3: 依次写入链路信息（起始节点，终止节点，请求的带宽资源，时延）
-            for line in lines[-num_edges:]:
-                from_id, to_id, length, a = [int(x) for x in line.split()]
-                resource = random.random() * LINK_CAPACITY * 0.5
-                delay = dis(coordinates[from_id], coordinates[to_id])
-                req_file.write("%d %d %f %f\n" % (from_id, to_id, resource, delay))
+        time = start + ((count_k + 1) / (k + 1)) * interval
+        duration = MIN_DURATION + int(-math.log(random.random()) * (DURATION_MEAN - MIN_DURATION))
+        node_amount = random.randint(min_num_nodes, max_num_nodes)
+        make_req(i, min_res, max_res, node_amount, time, duration)
 
 
 def extract_network(path, filename):
@@ -270,7 +214,7 @@ def extract_network(path, filename):
     else:
         """create a virtual network"""
 
-        node_num, link_num, time, duration, maxD = [int(x) for x in lines[0].split()]
+        node_num, link_num, time, duration, max_dis = [int(x) for x in lines[0].split()]
         graph = nx.Graph(type=0, time=time, duration=duration)
         for line in lines[1:node_num + 1]:
             x, y, c = [float(x) for x in line.split()]
@@ -306,12 +250,11 @@ def simulate_events(path, number):
 
 
 if __name__ == '__main__':
-
     # 生成节点数为100，连通率为0.5的随机型物理网络
-    # make_sub_wm(100, 0.5)
+    # make_sub_wm(100, 0.5, 50, 100)
 
     # 生成节点数为1×4×(1+3×8)=100，连通率为0.5的Transit-Stub型物理网络
-    make_sub_ts(1, 3, 4, 0.5, 8, 0.5)
+    make_sub_ts(1, 3, 4, 0.5, 8, 0.5, 50, 100)
 
-    # 平均每1000个时间单位内到达40个虚拟网络请求, 且虚拟节点数服从3~20的均匀分布
-    make_req(40, 3, 20)
+    # 平均每1000个时间单位内到达40个虚拟网络请求， 且虚拟节点数服从3~20的均匀分布，请求资源服从0~50的均匀分布
+    # make_batch_req(40, 3, 20, 0, 50)
